f2d1714fb2b52923fefd8e36adeb9d93139e8a79
DaveMcW <davemcw@gmail.com>
2019-06-21 03:32:05 -0400

    add diagonal rails

--- control.lua
+++ control.lua
@@ -1,8 +1,24 @@
+local MY_BRIDGES = {
+  ["rail-bridge"] = true,
+  ["rail-bridge-diagonal-left"] = true,
+  ["rail-bridge-diagonal-right"] = true,
+}
+local MY_RAILS = {
+  ["rail-bridge-north"] = true,
+  ["rail-bridge-east"] = true,
+  ["rail-bridge-rail-1"] = true,
+  ["rail-bridge-rail-2"] = true,
+  ["rail-bridge-rail-3"] = true,
+  ["rail-bridge-rail-4"] = true,
+}
+
 function on_init()
   -- Unlock recipe
   for _, force in pairs(game.forces) do
     if force.technologies["logistics-3"].researched then
       force.recipes["rail-bridge"].enabled = true
+      force.recipes["rail-bridge-diagonal-left"].enabled = true
+      force.recipes["rail-bridge-diagonal-right"].enabled = true
     end
   end
 end
@@ -10,48 +26,192 @@ end
 function on_built(event)
   local entity = event.created_entity or event.entity
   if not entity or not entity.valid then return end
+  if not MY_BRIDGES[entity.name] then return end
+
+  -- Align to rail grid
+  local mod_x = 0
+  local mod_y = 0
+  if entity.name == "rail-bridge-diagonal-left" or entity.name == "rail-bridge-diagonal-right" then
+    entity.get_or_create_control_behavior().enabled = false
+    if entity.direction % 4 == defines.direction.north then
+      mod_x = 0
+      mod_y = 1
+    else
+      mod_x = 1
+      mod_y = 0
+    end
+  end
+  local x = entity.position.x
+  local y = entity.position.y
+  if x % 2 == mod_x then x = x - 1 end
+  if y % 2 == mod_y then y = y - 1 end
+  if entity.position.x ~= x or entity.position.y ~= y then
+    entity.teleport{x, y}
+  end
+
+  -- Check collisions
+  local delete_rails = {}
+  for _, obstacle in pairs(entity.surface.find_entities_filtered{
+    type = {"straight-rail", "entity-ghost"},
+    area = area_under(entity),
+  }) do
+    if obstacle.type == "straight-rail"
+    and not MY_RAILS[obstacle.name]
+    and obstacle.force == entity.force
+    and obstacle.can_be_destroyed() then
+      table.insert(delete_rails, obstacle)
+    elseif obstacle.type == "entity-ghost"
+    and obstacle.ghost_type == "straight-rail" then
+      table.insert(delete_rails, obstacle)
+    else
+      -- Obstacle can't be destroyed, abort the build!
+      refund_entity(entity, event, obstacle)
+      return
+    end
+  end
+
+  -- Destroy rails in the area
+  for _, rail in pairs(delete_rails) do
+    if not MY_RAILS[rail.name] then
+      refund_entity(rail, event)
+    end
+  end
+
+  -- Create crossing rails
   if entity.name == "rail-bridge" then
-    -- Align to rail grid
-    local x = entity.position.x
-    local y = entity.position.y
-    if x % 2 == 0 then x = x - 1 end
-    if y % 2 == 0 then y = y - 1 end
-    if entity.position.x ~= x or entity.position.y ~= y then
-      entity.teleport{x, y}
+    create_rail("rail-bridge-north", entity, defines.direction.north)
+    create_rail("rail-bridge-east", entity, defines.direction.east)
+
+  elseif entity.name == "rail-bridge-diagonal-left" then
+    if entity.direction % 4 == defines.direction.north then
+      create_rail("rail-bridge-rail-1", entity, defines.direction.north, {0, -1})
+      create_rail("rail-bridge-rail-2", entity, defines.direction.north, {0, 1})
+      create_rail("rail-bridge-rail-1", entity, defines.direction.southwest, {0, -1})
+      create_rail("rail-bridge-rail-1", entity, defines.direction.northeast, {0, 1})
+    else
+      create_rail("rail-bridge-rail-1", entity, defines.direction.east, {-1, 0})
+      create_rail("rail-bridge-rail-2", entity, defines.direction.east, {1, 0})
+      create_rail("rail-bridge-rail-1", entity, defines.direction.southeast, {-1, 0})
+      create_rail("rail-bridge-rail-2", entity, defines.direction.northwest, {1, 0})
     end
 
-    -- Create rail crossing
-    entity.surface.create_entity{
-      name = "rail-bridge-north",
-      direction = defines.direction.north,
-      force = entity.force,
-      position = entity.position,
-    }
-    entity.surface.create_entity{
-      name = "rail-bridge-east",
-      direction = defines.direction.east,
-      force = entity.force,
-      position = entity.position,
-    }
+  elseif entity.name == "rail-bridge-diagonal-right" then
+    if entity.direction % 4 == defines.direction.north then
+      create_rail("rail-bridge-rail-3", entity, defines.direction.north, {0, -1})
+      create_rail("rail-bridge-rail-4", entity, defines.direction.north, {0, 1})
+      create_rail("rail-bridge-rail-3", entity, defines.direction.southeast, {0, -1})
+      create_rail("rail-bridge-rail-3", entity, defines.direction.northwest, {0, 1})
+    else
+      create_rail("rail-bridge-rail-4", entity, defines.direction.east, {-1, 0})
+      create_rail("rail-bridge-rail-3", entity, defines.direction.east, {1, 0})
+      create_rail("rail-bridge-rail-4", entity, defines.direction.northeast, {-1, 0})
+      create_rail("rail-bridge-rail-3", entity, defines.direction.southwest, {1, 0})
+    end
   end
 end
 
 function on_destroyed(event)
   local entity = event.entity
   if not entity or not entity.valid then return end
-  if entity.name == "rail-bridge" then
-    -- Remove rail crossing
-    local p = entity.position
+  if MY_BRIDGES[entity.name] then
+    -- Remove rails
     for _, rail in pairs(entity.surface.find_entities_filtered {
       type = "straight-rail",
-      area = {{p.x - 1, p.y - 1}, {p.x + 1, p.y + 1}},
+      area = area_under(entity),
+      force = entity.force,
     }) do
-      rail.destroy()
+      rail.destroy{raise_destroy = true}
     end
   end
 end
 
+function on_gui_opened(event)
+  -- Disable combinator gui
+  if event.entity and MY_BRIDGES[event.entity.name] then
+    game.players[event.player_index].opened = nil
+  end
+end
+
+function create_rail(name, bridge, direction, position)
+  if not position then position = {0, 0} end
+  local rail = bridge.surface.create_entity{
+    name = name,
+    direction = direction,
+    force = bridge.force,
+    position = {bridge.position.x + position[1], bridge.position.y + position[2]},
+  }
+  if rail then rail.destructible = false end
+end
+
+function area_under(entity)
+  local p = entity.position
+  local box = entity.prototype.collision_box
+  local dx = (box.right_bottom.x - box.left_top.x) / 2
+  local dy = (box.right_bottom.y - box.left_top.y) / 2
+  if entity.direction and entity.direction % 4 ~= defines.direction.north then
+    dx, dy = dy, dx
+  end
+  return {{p.x - dx, p.y - dy}, {p.x + dx, p.y + dy}}
+end
+
+function refund_entity(entity, build_event, colliding_entity)
+  -- Show alert
+  local player = nil
+  if build_event and build_event.player_index then
+    player = game.players[build_event.player_index]
+  end
+  if player and colliding_entity then
+    entity.surface.create_entity{
+      name = "flying-text",
+      text = {"cant-build-reason.entity-in-the-way", colliding_entity.localised_name},
+      position = entity.position,
+      render_player_index = build_event.player_index,
+    }
+  end
+
+  if entity.prototype.items_to_place_this then
+    -- Find the item used to place the entity
+    local item_count = 0
+    local item_name = nil
+    if build_event and build_event.stack and build_event.stack.valid_for_read then
+      item_name = build_event.stack.name
+    end
+    for _, item in pairs(entity.prototype.items_to_place_this) do
+      if item_name == item.name then
+        item_count = item.count
+        break
+      end
+    end
+    if item_count == 0 then
+      local item = entity.prototype.items_to_place_this[1]
+      if item then
+        item_name = item.name
+        item_count = item.count
+      end
+    end
+    local health = entity.health / entity.prototype.max_health
+
+    -- Return item to player inventory
+    if item_count > 0 and player then
+      local result = player.insert{name=item_name, count=item_count, health=health}
+      item_count = item_count - result
+    end
+
+    -- Return item to ground
+    if item_count > 0 then
+      entity.surface.spill_item_stack(entity.position, {name=item_name, count=item_count, health=health}, false, entity.force, false)
+    end
+  end
+  -- Destroy entity
+  if MY_BRIDGES[entity.name] then
+    entity.destroy()
+  else
+    entity.destroy{raise_destroy = true}
+  end
+end
+
 script.on_init(on_init)
+script.on_configuration_changed(on_init)
 script.on_event(defines.events.on_built_entity, on_built)
 script.on_event(defines.events.on_robot_built_entity, on_built)
 script.on_event(defines.events.on_entity_cloned, on_built)
@@ -61,3 +221,20 @@ script.on_event(defines.events.on_player_mined_entity, on_destroyed)
 script.on_event(defines.events.on_robot_mined_entity, on_destroyed)
 script.on_event(defines.events.on_entity_died, on_destroyed)
 script.on_event(defines.events.script_raised_destroy, on_destroyed)
+script.on_event(defines.events.on_gui_opened, on_gui_opened)
+
+function circle(entity, color, time, filled)
+  if not entity then return end
+  if not color then color = {g=1} end
+  if not time then time = 60 end
+  rendering.draw_circle{
+    surface = entity.surface,
+    target = entity,
+    forces = {entity.force},
+    color = color,
+    radius = 1,
+    width = 5,
+    filled = filled,
+    time_to_live = time,
+  }
+end
--- data.lua
+++ data.lua
@@ -3,7 +3,13 @@ local function cut_off_bottom(picture, bottom)
   local top = -picture.height / 2
   if top < bottom then
     picture.height = bottom - top
-    picture.shift = util.by_pixel(0, (top + picture.height/2) * picture.scale)
+    local shift = util.by_pixel(0, (top + picture.height/2) * picture.scale)
+    if not picture.shift then
+      picture.shift = shift
+    else
+      picture.shift[1] = picture.shift[1] + shift[1]
+      picture.shift[2] = picture.shift[2] + shift[2]
+    end
   else
     picture.filename = "__core__/graphics/empty.png"
     picture.width = 1
@@ -11,17 +17,26 @@ local function cut_off_bottom(picture, bottom)
   end
 end
 
--- Custom horizontal rail
+local function copy_and_shift(source, shift)
+  local picture = table.deepcopy(source)
+  if not picture.shift then picture.shift = {0, 0} end
+  if not picture.hr_version.shift then picture.hr_version.shift = {0, 0} end
+  picture.shift[1] = picture.shift[1] + shift[1]
+  picture.shift[2] = picture.shift[2] + shift[2]
+  picture.hr_version.shift[1] = picture.hr_version.shift[1] + shift[1]
+  picture.hr_version.shift[2] = picture.hr_version.shift[2] + shift[2]
+  return picture
+end
+
 local rail_east = table.deepcopy(data.raw["straight-rail"]["straight-rail"])
 rail_east.name = "rail-bridge-east"
-rail_east.collision_mask = {}
 rail_east.collision_box = {{-0.4, -0.99}, {0.4, 0.1}}
+rail_east.collision_mask = {}
 rail_east.minable = nil
 rail_east.flags = {"building-direction-8-way", "not-deconstructable", "not-upgradable"}
 rail_east.selectable_in_game = false
 data:extend{rail_east}
 
--- Custom vertical rail
 local rail_north = table.deepcopy(rail_east)
 rail_north.name = "rail-bridge-north"
 rail_north.collision_box = {{-0.4, -0.1}, {0.4, 0.99}}
@@ -29,11 +44,54 @@ for _, layer in pairs{"stone_path_background", "stone_path", "ties", "backplates
   cut_off_bottom(rail_north.pictures.straight_rail_vertical[layer], -14)
   cut_off_bottom(rail_north.pictures.straight_rail_vertical[layer].hr_version, -28)
 end
-for _, picture in pairs(rail_north.pictures.rail_endings.sheets) do
-  cut_off_bottom(picture, 18)
-  cut_off_bottom(picture.hr_version, 36)
-end
+-- Manually edited files with the following changes. Double the dimensions for hr_version.
+-- 1. Cut out rectangle at {{0,82}, {128,128}}
+-- 2. Cut out rectangle at {{512,0}, {640,54}}
+rail_north.pictures.rail_endings.sheets[1].filename = "__rail-bridge__/graphics/rail-endings-background.png"
+rail_north.pictures.rail_endings.sheets[1].hr_version.filename = "__rail-bridge__/graphics/hr-rail-endings-background.png"
+rail_north.pictures.rail_endings.sheets[2].filename = "__rail-bridge__/graphics/rail-endings-metals.png"
+rail_north.pictures.rail_endings.sheets[2].hr_version.filename = "__rail-bridge__/graphics/hr-rail-endings-metals.png"
 data:extend{rail_north}
+
+local rail_1 = table.deepcopy(rail_east);
+rail_1.name = "rail-bridge-rail-1"
+rail_1.localised_name = {"entity-name.rail-bridge-diagonal-left"}
+rail_1.collision_box = {{-0.1, -0.99}, {0.1, 0.1}}
+for _, layer in pairs{"stone_path_background", "stone_path", "ties", "backplates", "metals"} do
+  cut_off_bottom(rail_1.pictures.straight_rail_diagonal_right_bottom[layer], 2)
+  cut_off_bottom(rail_1.pictures.straight_rail_diagonal_right_bottom[layer].hr_version, 4)
+end
+data:extend{rail_1}
+
+local rail_2 = table.deepcopy(rail_east);
+rail_2.name = "rail-bridge-rail-2"
+rail_2.localised_name = {"entity-name.rail-bridge-diagonal-left"}
+rail_2.collision_box = {{-0.1, -0.1}, {0.1, 0.99}}
+for _, layer in pairs{"stone_path_background", "stone_path", "ties", "backplates", "metals"} do
+  cut_off_bottom(rail_2.pictures.straight_rail_diagonal_left_top[layer], -30)
+  cut_off_bottom(rail_2.pictures.straight_rail_diagonal_left_top[layer].hr_version, -60)
+end
+data:extend{rail_2}
+
+local rail_3 = table.deepcopy(rail_east)
+rail_3.name = "rail-bridge-rail-3"
+rail_3.localised_name = {"entity-name.rail-bridge-diagonal-right"}
+rail_3.collision_box = {{-0.1, -0.99}, {0.1, 0.1}}
+for _, layer in pairs{"stone_path_background", "stone_path", "ties", "backplates", "metals"} do
+  cut_off_bottom(rail_3.pictures.straight_rail_diagonal_left_bottom[layer], 2)
+  cut_off_bottom(rail_3.pictures.straight_rail_diagonal_left_bottom[layer].hr_version, 4)
+end
+data:extend{rail_3}
+
+local rail_4 = table.deepcopy(rail_east)
+rail_4.name = "rail-bridge-rail-4"
+rail_4.localised_name = {"entity-name.rail-bridge-diagonal-right"}
+rail_4.collision_box = {{-0.1, -0.1}, {0.1, 0.99}}
+for _, layer in pairs{"stone_path_background", "stone_path", "ties", "backplates", "metals"} do
+  cut_off_bottom(rail_4.pictures.straight_rail_diagonal_right_top[layer], -30)
+  cut_off_bottom(rail_4.pictures.straight_rail_diagonal_right_top[layer].hr_version, -60)
+end
+data:extend{rail_4}
 
 -- Bridge entity
 local bridge = {
@@ -41,12 +99,12 @@ local bridge = {
   name = "rail-bridge",
   icon = "__rail-bridge__/graphics/icon.png",
   icon_size = 32,
-  order = "a[train-system]-a[rail]-b[bridge]",
   subgroup = "transport",
+  order = "a[train-system]-a[rail]-b[bridge]",
   flags = {"placeable-neutral", "player-creation"},
   collision_mask = {"item-layer", "object-layer", "water-tile"},
   collision_box = {{-0.9, -0.9}, {0.9, 0.9}},
-  selection_box = {{-1.2, -1}, {1.2, 1.6}},
+  selection_box = {{-1, -1}, {1, 1}},
   minable = {mining_time = 0.5, result = "rail-bridge"},
   max_health = 300,
   resistances = table.deepcopy(rail_east.resistances),
@@ -60,13 +118,11 @@ for _, layer in pairs{"stone_path_background", "stone_path", "ties"} do
 end
 table.insert(bridge.picture.layers, {
   filename = "__rail-bridge__/graphics/bridge.png",
-  priority = "extra-high",
   width = 72,
   height = 32,
   shift = util.by_pixel(0, 34),
   hr_version = {
     filename = "__rail-bridge__/graphics/hr-bridge.png",
-    priority = "extra-high",
     width = 144,
     height = 64,
     shift = util.by_pixel(0, 34),
@@ -79,37 +135,226 @@ for _, layer in pairs{"backplates", "metals"} do
 end
 data:extend{bridge}
 
--- Bridge item
-local item = {
-  type = "item",
-  name = bridge.name,
-  place_result = bridge.name,
-  icon = bridge.icon,
-  icon_size = bridge.icon_size,
-  subgroup = bridge.subgroup,
-  order = bridge.order,
-  flags = {},
-  stack_size = 10,
-}
-data:extend{item}
+-- Diagonal left bridge entity
+local diag_left = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"]);
+diag_left.name = "rail-bridge-diagonal-left"
+diag_left.icon = "__rail-bridge__/graphics/icon.png"
+diag_left.icon_size = 32
+diag_left.subgroup = "transport"
+diag_left.order = "a[train-system]-a[rail]-b[diagonal-bridge]"
+diag_left.flags = {"placeable-neutral", "player-creation", "hide-alt-info"}
+diag_left.fast_replaceable_group = nil
+diag_left.collision_mask = {"item-layer", "object-layer", "water-tile"}
+diag_left.collision_box = {{-0.55, -1.55}, {0.55, 1.55}}
+diag_left.selection_box = {{-1, -2}, {1, 2}}
+diag_left.minable = { mining_time = 0.5, result = "rail-bridge-diagonal-left" }
+diag_left.max_health = 300
+diag_left.item_slot_count = 0
+diag_left.circuit_wire_max_distance = 0
+diag_left.activity_led_sprites = {}
+diag_left.activity_led_light_offsets = {}
+diag_left.circuit_wire_connection_points = {}
+for _, direction in pairs{"north", "east", "south", "west"} do
+  diag_left.activity_led_sprites[direction] = {
+    filename = "__core__/graphics/empty.png",
+    width = 1,
+    height = 1,
+  }
+  table.insert(diag_left.activity_led_light_offsets, {0,0})
+  table.insert(diag_left.circuit_wire_connection_points, {
+    wire = {green={0,0}, red={0,0}},
+    shadow = {green={0,0}, red={0,0}},
+  })
+  diag_left.sprites[direction] = {layers = {}}
+end
+for _, layer in pairs{"stone_path_background", "stone_path", "ties"} do
+  for _, direction in pairs{"east", "west"} do
+    table.insert(diag_left.sprites[direction].layers, copy_and_shift(rail_1.pictures.straight_rail_diagonal_right_bottom[layer], {-1, 0}))
+    table.insert(diag_left.sprites[direction].layers, copy_and_shift(rail_2.pictures.straight_rail_diagonal_left_top[layer], {1, 0}))
+    table.insert(diag_left.sprites[direction].layers, copy_and_shift(rail_east.pictures.straight_rail_horizontal[layer], {-1, 0}))
+    table.insert(diag_left.sprites[direction].layers, copy_and_shift(rail_east.pictures.straight_rail_horizontal[layer], {1, 0}))
+  end
+end
+for _, direction in pairs{"north", "east", "south", "west"} do
+  if direction == "north" or direction == "south" then
+    table.insert(diag_left.sprites[direction].layers, {
+      filename = "__rail-bridge__/graphics/bridge.png",
+      width = 72,
+      height = 32,
+      shift = util.by_pixel(0, 34),
+      hr_version = {
+        filename = "__rail-bridge__/graphics/hr-bridge.png",
+        width = 144,
+        height = 64,
+        shift = util.by_pixel(0, 34),
+        scale = 0.5,
+      }
+    })
+  else
+    table.insert(diag_left.sprites[direction].layers, {
+      filename = "__rail-bridge__/graphics/hr-bridge-sw.png",
+      width = 72,
+      height = 32,
+      shift = util.by_pixel(-32, 34),
+      hr_version = {
+        filename = "__rail-bridge__/graphics/hr-bridge-sw.png",
+        width = 144,
+        height = 64,
+        shift = util.by_pixel(-32, 34),
+        scale = 0.5,
+      }
+    })
+  end
+end
+for _, layer in pairs{"backplates", "metals"} do
+  for _, direction in pairs{"east", "west"} do
+    table.insert(diag_left.sprites[direction].layers, copy_and_shift(rail_1.pictures.straight_rail_diagonal_right_bottom[layer], {-1, 0}))
+    table.insert(diag_left.sprites[direction].layers, copy_and_shift(rail_2.pictures.straight_rail_diagonal_left_top[layer], {1, 0}))
+    table.insert(diag_left.sprites[direction].layers, copy_and_shift(rail_east.pictures.straight_rail_horizontal[layer], {-1, 0}))
+    table.insert(diag_left.sprites[direction].layers, copy_and_shift(rail_east.pictures.straight_rail_horizontal[layer], {1, 0}))
+  end
+end
+data:extend{diag_left}
 
--- Bridge recipe
-local recipe = {
-  type = "recipe",
-  name = "rail-bridge",
-  result = "rail-bridge",
-  enabled = false,
-  energy_required = 10,
-  ingredients = {
-    {"concrete", 100},
-    {"steel-plate", 20},
-    {"rail", 2},
+-- Diagonal right bridge entity
+local diag_right = table.deepcopy(diag_left)
+diag_right.name = "rail-bridge-diagonal-right"
+diag_right.icon = "__rail-bridge__/graphics/icon.png"
+diag_right.minable.result = "rail-bridge-diagonal-right"
+for _, direction in pairs{"north", "east", "south", "west"} do
+  diag_right.sprites[direction] = {layers = {}}
+end
+for _, layer in pairs{"stone_path_background", "stone_path", "ties"} do
+  for _, direction in pairs{"east", "west"} do
+    table.insert(diag_right.sprites[direction].layers, copy_and_shift(rail_4.pictures.straight_rail_diagonal_right_top[layer], {-1, 0}))
+    table.insert(diag_right.sprites[direction].layers, copy_and_shift(rail_3.pictures.straight_rail_diagonal_left_bottom[layer], {1, 0}))
+    table.insert(diag_right.sprites[direction].layers, copy_and_shift(rail_east.pictures.straight_rail_horizontal[layer], {-1, 0}))
+    table.insert(diag_right.sprites[direction].layers, copy_and_shift(rail_east.pictures.straight_rail_horizontal[layer], {1, 0}))
+  end
+end
+for _, direction in pairs{"north", "east", "south", "west"} do
+  if direction == "north" or direction == "south" then
+    table.insert(diag_right.sprites[direction].layers, {
+      filename = "__rail-bridge__/graphics/bridge.png",
+      width = 72,
+      height = 32,
+      shift = util.by_pixel(0, 34),
+      hr_version = {
+        filename = "__rail-bridge__/graphics/hr-bridge.png",
+        width = 144,
+        height = 64,
+        shift = util.by_pixel(0, 34),
+        scale = 0.5,
+      }
+    })
+  else
+    table.insert(diag_right.sprites[direction].layers, {
+      filename = "__rail-bridge__/graphics/hr-bridge-se.png",
+      width = 72,
+      height = 32,
+      shift = util.by_pixel(32, 34),
+      hr_version = {
+        filename = "__rail-bridge__/graphics/hr-bridge-se.png",
+        width = 144,
+        height = 64,
+        shift = util.by_pixel(32, 34),
+        scale = 0.5,
+      }
+    })
+  end
+end
+for _, layer in pairs{"backplates", "metals"} do
+  for _, direction in pairs{"east", "west"} do
+    table.insert(diag_right.sprites[direction].layers, copy_and_shift(rail_4.pictures.straight_rail_diagonal_right_top[layer], {-1, 0}))
+    table.insert(diag_right.sprites[direction].layers, copy_and_shift(rail_3.pictures.straight_rail_diagonal_left_bottom[layer], {1, 0}))
+    table.insert(diag_right.sprites[direction].layers, copy_and_shift(rail_east.pictures.straight_rail_horizontal[layer], {-1, 0}))
+    table.insert(diag_right.sprites[direction].layers, copy_and_shift(rail_east.pictures.straight_rail_horizontal[layer], {1, 0}))
+  end
+end
+data:extend{diag_right};
+
+data:extend {
+  {
+    type = "item",
+    name = bridge.name,
+    place_result = bridge.name,
+    icon = bridge.icon,
+    subgroup = bridge.subgroup,
+    icon_size = bridge.icon_size,
+    order = bridge.order,
+    flags = {},
+    stack_size = 10,
+  },
+  {
+    type = "item",
+    name = diag_left.name,
+    place_result = diag_left.name,
+    icon = diag_left.icon,
+    subgroup = diag_left.subgroup,
+    icon_size = diag_left.icon_size,
+    order = diag_left.order,
+    flags = {},
+    stack_size = 10,
+  },
+  {
+    type = "item",
+    name = diag_right.name,
+    place_result = diag_right.name,
+    icon = diag_right.icon,
+    subgroup = diag_right.subgroup,
+    icon_size = diag_right.icon_size,
+    order = diag_right.order,
+    flags = {},
+    stack_size = 10,
+  },
+  {
+    type = "recipe",
+    name = "rail-bridge",
+    result = "rail-bridge",
+    enabled = false,
+    energy_required = 10,
+    ingredients = {
+      {"concrete", 100},
+      {"steel-plate", 20},
+      {"rail", 2},
+    },
+  },
+  {
+    type = "recipe",
+    name = "rail-bridge-diagonal-left",
+    result = "rail-bridge-diagonal-left",
+    enabled = false,
+    energy_required = 10,
+    ingredients = {
+      {"concrete", 100},
+      {"steel-plate", 20},
+      {"rail", 4},
+    },
+  },
+  {
+    type = "recipe",
+    name = "rail-bridge-diagonal-right",
+    result = "rail-bridge-diagonal-right",
+    enabled = false,
+    energy_required = 10,
+    ingredients = {
+      {"concrete", 100},
+      {"steel-plate", 20},
+      {"rail", 4},
+    },
   },
 }
-data:extend{recipe}
 
--- Bridge is unlocked with Logistics 3
+-- Recipes are unlocked with Logistics 3
 table.insert(data.raw.technology["logistics-3"].effects, {
   type = "unlock-recipe",
   recipe = "rail-bridge",
+})
+table.insert(data.raw.technology["logistics-3"].effects, {
+  type = "unlock-recipe",
+  recipe = "rail-bridge-diagonal-left",
+})
+table.insert(data.raw.technology["logistics-3"].effects, {
+  type = "unlock-recipe",
+  recipe = "rail-bridge-diagonal-right",
 })
